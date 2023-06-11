ffcf938967d67278b2a95a576a6c52a5408b2d85
tin <tin.pt@ntnc.vn>
2023-06-11 23:45:48 +0700

    initial commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let images = ["dice_1.png","dice_2.png","dice_3.png","dice_4.png","dice_5.png","dice_6.png",]
+    
+    @IBAction func onAskPressed(_ sender: Any) {
+        imageView.image = UIImage(imageLiteralResourceName: images.randomElement()!)
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
